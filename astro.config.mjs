110cbefe5db587530538863549d95fa185b436ee
arianrdcas <arianrdcas@gmail.com>
2024-07-03 16:53:40 -0400

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://arianrdcas.github.io",
   devToolbar: {
-    enabled: false
+    enabled: false,
   },
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
