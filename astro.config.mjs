d48fd23f0792dfafbaba542b87f194fc44dac997
arianrdcas <arianrdcas@gmail.com>
2024-07-10 12:02:41 -0400

    deploy done

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://arianrdcas.github.io",
+  base: "/Andy",
   devToolbar: {
     enabled: false,
   },
