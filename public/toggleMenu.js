a9a540cf6f54fb971dfb71ac13191831b74cc93c
arianrdcas <arianrdcas@gmail.com>
2024-07-17 17:16:35 -0400

    Corregido cambio de svg en boton de menu

--- public/toggleMenu.js
+++ public/toggleMenu.js
@@ -1,15 +1,25 @@
 // toggleMenu.js
 
-// Selecciona el botón y el menú usando querySelector y añade tipos explícitos
-const boton = document.querySelector("#boton");
-const menu = document.querySelector("#menu");
+document.addEventListener("DOMContentLoaded", () => {
+  // Selecciona el botón y el menú usando querySelector y añade tipos explícitos
+  const boton = document.querySelector("#boton");
+  const menu = document.querySelector("#menu");
 
-// Verifica que los elementos no sean nulos antes de agregar el event listener
-if (boton && menu) {
-  boton.addEventListener("click", () => {
-    console.log("click");
-    menu.classList.toggle("hidden");
-  });
-} else {
-  console.error("El botón o el menú no se encontraron en el DOM.");
-}
+  // Selecciona los íconos de hamburguesa y de cruz
+  const hamburgerIcon = document.querySelector("#hamburger-icon");
+  const closeIcon = document.querySelector("#close-icon");
+
+  // Verifica que los elementos no sean nulos antes de agregar el event listener
+  if (boton && menu && hamburgerIcon && closeIcon) {
+    boton.addEventListener("click", () => {
+      console.log("click");
+      menu.classList.toggle("hidden");
+      hamburgerIcon.classList.toggle("hidden");
+      closeIcon.classList.toggle("hidden");
+    });
+  } else {
+    console.error(
+      "El botón, el menú o los íconos no se encontraron en el DOM."
+    );
+  }
+});
